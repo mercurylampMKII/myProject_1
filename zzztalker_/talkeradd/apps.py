7cc6610fe67cc453ee0d37dda7f4fbeb50fd58a2
book <bookmkii@test.com>
2018-10-23 21:26:35 +0800

    dev-delete-zzztalker_

--- zzztalker_/talkeradd/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TalkeraddConfig(AppConfig):
-    name = 'talkeradd'
--- zzztalker_/talkeradd/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('bg-manage-add/', views.index),
-    path('add/',views.add),
-
-]
